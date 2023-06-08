e1b2bd6422c5c13e5df115b07e031ff4c947d0c7
Justin Rich <jrich@cyral.com>
2023-06-08 15:58:13 -0700

    cleanup

--- logattach.sh
+++ logattach.sh
@@ -1,25 +1,39 @@
 #!/usr/bin/env bash
 
-echo "EMAIL: $EMAIL"
-echo "ACCOUNTID: $ACCOUNTID"
-echo "APP_NAME: ${APP_NAME}"
-echo "PR_URL: ${PR_URL}"
-echo "DETAILS_URL: ${DETAILS_URL}"
-echo "CHECK_URL: ${CHECK_URL}"
+# extract details from check run
+checkDetails=$(gh api --method GET -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "${CHECK_URL}" | jq -Rnr '[inputs] | join("\\n") | fromjson | .')
+logUrl=$(echo -E "$checkDetails" | jq -r '.output.summary' | grep -oP '\[View logs\]\(\K.*(?=\))')
+buildId=$(echo "$logUrl" | awk -F/ '{print $(NF)}')
+appName=$(echo "$logUrl" | awk -F/ '{print $(NF-1)}')
+prNumber=$(echo -E "$checkDetails" | jq '.pull_requests[0].number')
 
-url="https://api.cloudflare.com/client/v4/accounts/${ACCOUNTID}/pages/projects/${APP_NAME}/deployments/90075c2d-ffd3-44c2-a516-cf962416884a/history/logs"
+# collect cloudflare logs
+url="https://api.cloudflare.com/client/v4/accounts/${ACCOUNTID}/pages/projects/${appName}/deployments/${buildId}/history/logs"
 
 logData=$(curl --request GET \
-  --url $url \
+  --url "$url" \
   --header 'Content-Type: application/json' \
   --header "X-Auth-Email: ${EMAIL}" \
-  --header "X-Auth-Key: ${API_KEY}" | jq -r '.result.data | map("\(.ts) | \(.line)")[]' | awk -F ' [|] ' '{printf "%-27s | %s\n", $1, $2}')
+  --header "X-Auth-Key: ${API_KEY}" | jq -r '.result.data | map("\(.ts) | \(.line)")[]' | awk -F ' [|] ' '{printf "%-27s | %s\n", $1, $2}' )
 
-echo "Log data"
-echo "------------"
+# find PR comment (its technically an issue)
+## get first match, should only be one, but this will be safer
+issue=$(gh api --method GET -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/${GITHUB_REPOSITORY}/issues/${prNumber}/comments" |jq -Rnr '[inputs] | join("\\n") | fromjson | first(.[] | select(.user.login | test("cloudflare")))')
+issueUrl=$(echo -E "$issue" | jq -r .url)
+body=$(echo -E "$issue" | jq -r .body | sed '/<details>/,/<\/details>/d') # remove previously attached log
+newBody="$body
+<details><summary>Log Output</summary>
+
+\`\`\`console
+$logData
+\`\`\`
+
+</details>"
+issueReturn=$(gh api --method PATCH -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "$issueUrl" -f body="$newBody")
+
+
+echo "========== job complete ========="
+echo "----------  Issue Patch Data ----------"
+echo "$issueReturn" | jq
+echo "---------- Log Output ----------"
 echo "$logData"
-
-#gh api --method GET -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/cyralinc/cyral-docs-v2/check-runs/13378092863 | jq '.output.summary' | grep -o -P '(?<=\[View logs\]\().*?(?=\))'
-
-#var1=$(echo "$url" | awk -F/ '{print $(NF-1)}')
-#var2=$(echo "$url" | awk -F/ '{print $NF}')
